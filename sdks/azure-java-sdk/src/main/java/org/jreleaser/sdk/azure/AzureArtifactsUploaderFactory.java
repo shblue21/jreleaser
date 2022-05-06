e2d2ddf1829e6fab56f7693e526a04c36199e5a9
JIHUN KIM <jihunkimkw@gmail.com>
2022-05-06 16:21:23 +0900

    sdk name azuredevops -> azure

--- sdks/azure-java-sdk/src/main/java/org/jreleaser/sdk/azure/AzureArtifactsUploaderFactory.java
+++ sdks/azure-java-sdk/src/main/java/org/jreleaser/sdk/azure/AzureArtifactsUploaderFactory.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jreleaser.sdk.azuredevops;
+package org.jreleaser.sdk.azure;
 
 import org.jreleaser.model.AzureArtifacts;
 import org.jreleaser.model.JReleaserContext;
